62c9afdc5556b5f26b692bdb697dfa18f5d0966c
Twister0x <123463790+Twister0x@users.noreply.github.com>
2023-07-18 19:47:10 +0300

    Add files via upload

---
+++ card-master/admin.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    include "header.php";
+    ?>
+
+    <div class="mainWrapp">
+        <div class="addfunc">
+            <div><button>Add Product</button></div>
+            <div><button>Product List</button></div>
+            <div><button>Add Category</button></div>
+            <div><button>Add Brend</button></div>
+        </div>
+        <div class="brends">Brends</div>
+        <div class="shopEdit">
+            <div class="shopadmin">
+                <div class="card">
+                    <div class="img"><img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt=""></div>
+                    <div class="cardTitle">Lorem ipsum dolor sit amet.</div>
+                    <div class="cardDescr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur, eos omnis possimus reprehenderit exercitationem.</div>
+                    <div class="cardReducere">
+                        <div class="priceReducere">2.566 lei</div> <span class="productRed">-500 lei</span>
+                    </div>
+                    <div class="productinfo">
+                        <div class="price">2222 lei</div>
+                    </div>
+                    <div class="editButtons">
+                        <button class="edit">Edit</button>
+                        <button class="delete">Delete</button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img"><img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt=""></div>
+                    <div class="cardTitle">Lorem ipsum dolor sit amet.</div>
+                    <div class="cardDescr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur, eos omnis possimus reprehenderit exercitationem.</div>
+                    <div class="cardReducere">
+                        <div class="priceReducere">2.566 lei</div> <span class="productRed">-500 lei</span>
+                    </div>
+                    <div class="productinfo">
+                        <div class="price">2222 lei</div>
+                    </div>
+                    <div class="editButtons">
+                        <button class="edit">Edit</button>
+                        <button class="delete">Delete</button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img"><img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt=""></div>
+                    <div class="cardTitle">Lorem ipsum dolor sit amet.</div>
+                    <div class="cardDescr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur, eos omnis possimus reprehenderit exercitationem.</div>
+                    <div class="cardReducere">
+                        <div class="priceReducere">2.566 lei</div> <span class="productRed">-500 lei</span>
+                    </div>
+                    <div class="productinfo">
+                        <div class="price">2222 lei</div>
+                    </div>
+                    <div class="editButtons">
+                        <button class="edit">Edit</button>
+                        <button class="delete">Delete</button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img"><img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt=""></div>
+                    <div class="cardTitle">Lorem ipsum dolor sit amet.</div>
+                    <div class="cardDescr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur, eos omnis possimus reprehenderit exercitationem.</div>
+                    <div class="cardReducere">
+                        <div class="priceReducere">2.566 lei</div> <span class="productRed">-500 lei</span>
+                    </div>
+                    <div class="productinfo">
+                        <div class="price">2222 lei</div>
+                    </div>
+                    <div class="editButtons">
+                        <button class="edit">Edit</button>
+                        <button class="delete">Delete</button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img"><img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt=""></div>
+                    <div class="cardTitle">Lorem ipsum dolor sit amet.</div>
+                    <div class="cardDescr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur, eos omnis possimus reprehenderit exercitationem.</div>
+                    <div class="cardReducere">
+                        <div class="priceReducere">2.566 lei</div> <span class="productRed">-500 lei</span>
+                    </div>
+                    <div class="productinfo">
+                        <div class="price">2222 lei</div>
+                    </div>
+                    <div class="editButtons">
+                        <button class="edit">Edit</button>
+                        <button class="delete">Delete</button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img"><img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt=""></div>
+                    <div class="cardTitle">Lorem ipsum dolor sit amet.</div>
+                    <div class="cardDescr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur, eos omnis possimus reprehenderit exercitationem.</div>
+                    <div class="cardReducere">
+                        <div class="priceReducere">2.566 lei</div> <span class="productRed">-500 lei</span>
+                    </div>
+                    <div class="productinfo">
+                        <div class="price">2222 lei</div>
+                    </div>
+                    <div class="editButtons">
+                        <button class="edit">Edit</button>
+                        <button class="delete">Delete</button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img"><img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt=""></div>
+                    <div class="cardTitle">Lorem ipsum dolor sit amet.</div>
+                    <div class="cardDescr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur, eos omnis possimus reprehenderit exercitationem.</div>
+                    <div class="cardReducere">
+                        <div class="priceReducere">2.566 lei</div> <span class="productRed">-500 lei</span>
+                    </div>
+                    <div class="productinfo">
+                        <div class="price">2222 lei</div>
+                    </div>
+                    <div class="editButtons">
+                        <button class="edit">Edit</button>
+                        <button class="delete">Delete</button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img"><img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt=""></div>
+                    <div class="cardTitle">Lorem ipsum dolor sit amet.</div>
+                    <div class="cardDescr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur, eos omnis possimus reprehenderit exercitationem.</div>
+                    <div class="cardReducere">
+                        <div class="priceReducere">2.566 lei</div> <span class="productRed">-500 lei</span>
+                    </div>
+                    <div class="productinfo">
+                        <div class="price">2222 lei</div>
+                    </div>
+                    <div class="editButtons">
+                        <button class="edit">Edit</button>
+                        <button class="delete">Delete</button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img"><img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt=""></div>
+                    <div class="cardTitle">Lorem ipsum dolor sit amet.</div>
+                    <div class="cardDescr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur, eos omnis possimus reprehenderit exercitationem.</div>
+                    <div class="cardReducere">
+                        <div class="priceReducere">2.566 lei</div> <span class="productRed">-500 lei</span>
+                    </div>
+                    <div class="productinfo">
+                        <div class="price">2222 lei</div>
+                    </div>
+                    <div class="editButtons">
+                        <button class="edit">Edit</button>
+                        <button class="delete">Delete</button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img"><img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt=""></div>
+                    <div class="cardTitle">Lorem ipsum dolor sit amet.</div>
+                    <div class="cardDescr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur, eos omnis possimus reprehenderit exercitationem.</div>
+                    <div class="cardReducere">
+                        <div class="priceReducere">2.566 lei</div> <span class="productRed">-500 lei</span>
+                    </div>
+                    <div class="productinfo">
+                        <div class="price">2222 lei</div>
+                    </div>
+                    <div class="editButtons">
+                        <button class="edit">Edit</button>
+                        <button class="delete">Delete</button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img"><img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt=""></div>
+                    <div class="cardTitle">Lorem ipsum dolor sit amet.</div>
+                    <div class="cardDescr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur, eos omnis possimus reprehenderit exercitationem.</div>
+                    <div class="cardReducere">
+                        <div class="priceReducere">2.566 lei</div> <span class="productRed">-500 lei</span>
+                    </div>
+                    <div class="productinfo">
+                        <div class="price">2222 lei</div>
+                    </div>
+                    <div class="editButtons">
+                        <button class="edit">Edit</button>
+                        <button class="delete">Delete</button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img"><img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt=""></div>
+                    <div class="cardTitle">Lorem ipsum dolor sit amet.</div>
+                    <div class="cardDescr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur, eos omnis possimus reprehenderit exercitationem.</div>
+                    <div class="cardReducere">
+                        <div class="priceReducere">2.566 lei</div> <span class="productRed">-500 lei</span>
+                    </div>
+                    <div class="productinfo">
+                        <div class="price">2222 lei</div>
+                    </div>
+                    <div class="editButtons">
+                        <button class="edit">Edit</button>
+                        <button class="delete">Delete</button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img"><img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt=""></div>
+                    <div class="cardTitle">Lorem ipsum dolor sit amet.</div>
+                    <div class="cardDescr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur, eos omnis possimus reprehenderit exercitationem.</div>
+                    <div class="cardReducere">
+                        <div class="priceReducere">2.566 lei</div> <span class="productRed">-500 lei</span>
+                    </div>
+                    <div class="productinfo">
+                        <div class="price">2222 lei</div>
+                    </div>
+                    <div class="editButtons">
+                        <button class="edit">Edit</button>
+                        <button class="delete">Delete</button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img"><img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt=""></div>
+                    <div class="cardTitle">Lorem ipsum dolor sit amet.</div>
+                    <div class="cardDescr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur, eos omnis possimus reprehenderit exercitationem.</div>
+                    <div class="cardReducere">
+                        <div class="priceReducere">2.566 lei</div> <span class="productRed">-500 lei</span>
+                    </div>
+                    <div class="productinfo">
+                        <div class="price">2222 lei</div>
+                    </div>
+                    <div class="editButtons">
+                        <button class="edit">Edit</button>
+                        <button class="delete">Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="category">category</div>
+    </div>
+
+</body>
+
+</html>
